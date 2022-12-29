8a2c3aa1937ea836ebd039db2d853df158f9f9fe
Bessaheken Danielle Oriane <dabessah@stud.hs-heilbronn.de>
2022-12-29 03:51:48 +0100

    small changes

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def preprocess(image: Image.Image):
 
 
 if __name__ == "__main__":   
-    uvicorn.run(app, host = 'localhost', port = 8000, reload= True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080)
    
